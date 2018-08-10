02f49520828c68aad45ba64cfa6999c0df09e308
hablikR <41376238+hablikR@users.noreply.github.com>
2018-08-10 15:41:29 +0200

    Test writing in progress

--- src/test/java/customermanagement/customer/CustomerApplicationTests.java
+++ src/test/java/customermanagement/customer/CustomerApplicationTests.java
@@ -1,16 +1,32 @@
 package customermanagement.customer;
 
+import customermanagement.customer.controller.CustomerController;
+import customermanagement.customer.service.CustomerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CustomerApplicationTests {
 
+    private final CustomerService customerService = null;
+
+    @Autowired
+    CustomerController customerController = new CustomerController(customerService);
+
     @Test
-    public void contextLoads() {
+        public void contextLoads() throws Exception{
+            assertThat(customerController).isNotNull();
     }
 
+
+
+
+    
 }
+
